12176eb1892bdeb0a4a7b743eec7123daf68a950
PengRong <1557862201@qq.com>
2019-06-24 13:44:29 +0800

    基于策略模式消灭所有流程性的判断条件。

--- src/main/java/org/vincent/strategy/StrategyEnums.java
+++ src/main/java/org/vincent/strategy/StrategyEnums.java
@@ -4,7 +4,7 @@ package org.vincent.strategy;
  * @Package org.vincent.strategy
  * @ClassName StrategyEnums.java
  * @date 2019/6/10 - 14:58
- * @description : 策略接口类型定义 每个策略接口实现的枚举值；
+ * @description : 策略类型定义 每个枚举值都代表一个策略算法；
  * 用於枚舉不同的情況值
  * Created by PengRong .
  */
